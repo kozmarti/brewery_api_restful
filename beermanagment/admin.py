51a73be7c34a1850e528d8f2a511d01d3f7360d2
kozmarti <kozmarti@gmail.com>
2022-11-04 23:29:55 +0100

    fix #6

--- beermanagment/admin.py
+++ beermanagment/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from beermanagment.models import Bar, Reference, Stock, Order, OrderItem
 
+admin.site.register([Bar, Reference, Stock, Order, OrderItem])
 # Register your models here.
--- beermanagment/models.py
+++ beermanagment/models.py
@@ -6,12 +6,18 @@ class Reference(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=200, blank=True)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         ordering = ['reference']
 
 
 class Bar(models.Model):
     name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
 
     class Meta:
         ordering = ['id']
@@ -22,12 +28,18 @@ class Stock(models.Model):
     bar = models.ForeignKey("Bar", on_delete=models.CASCADE)
     stock = models.PositiveIntegerField()
 
+    def __str__(self):
+        return f'in bar "{self.bar.name}" {self.stock} bottle of "{self.reference.name}" is on stock'
+
     class Meta:
         ordering = ['id']
 
 
 class Order(models.Model):
     bar = models.ForeignKey("Bar", on_delete=models.CASCADE)
+
+    # def __str__(self):
+    #     return f'From bar "{self.bar.name}" beers ordered: {[i.reference.name for i in self.items.all()]} '
 
     class Meta:
         ordering = ['-id']
@@ -37,3 +49,6 @@ class OrderItem(models.Model):
     order = models.ForeignKey("Order", related_name='items', on_delete=models.CASCADE)
     reference = models.ForeignKey("Reference", on_delete=models.CASCADE)
     count = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.count} bottle(s) of {self.reference.name} beer ordered'
